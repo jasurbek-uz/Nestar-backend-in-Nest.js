3a36ff24e84d162abf8d146f35d4a46fdade84bf
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-08-17 15:06:49 +0900

    feat: connect MongoDb database

--- train.ts
+++ train.ts
@@ -6,7 +6,7 @@ MASALAN: reduceNestedArray([1, [1, 2, [4]]]) return 8
 */
 
 function sumArray(arr) {
-	return arr.flat(Infinity).reduce((sum, num) => sum + num, 0);
+  return arr.flat(Infinity).reduce((sum, num) => sum + num, 0);
 }
 
 console.log(sumArray([1, [1, 2, [4]]]));
