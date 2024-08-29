0415475f596cd92395b0d3fe4515920e85c0325d
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-08-29 17:00:09 +0900

    feat:develop single and multy image uploader mutation graphql api

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -57,7 +57,7 @@ export class MemberService {
 		const result: Member = await this.memberModel
 			.findOneAndUpdate({ _id: memberId, memberStatus: MemberStatus.ACTIVE }, input, { new: true })
 			.exec();
-		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+		if (!result) throw new InternalServerErrorException(Message.UPLOAD_FAILED);
 		result.accessToken = await this.authService.createToken(result);
 		return result;
 	}
@@ -115,7 +115,7 @@ export class MemberService {
 		const match: T = { };
 		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
 
-    if (memberStatus) match.memberStatus = memberStatus;
+    if (memberStatus) match.MemberStatus = memberStatus;
     if (memberType) match.memberType = memberType;
 		if (text) match.memberNick = { $regex: new RegExp(text, 'i') };
 		console.log('match:', match);
