73c7ab456096e5c2505442fbd80182587e7afdfc
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-08-30 19:29:21 +0900

    feat:develop create propertu graphql api

--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -5,9 +5,12 @@ import { MongooseModule } from "@nestjs/mongoose";
 import PropertySchema from "../../schemas/Property.model";
 import { AuthModule } from "../auth/auth.module";
 import { ViewModule } from "../view/view.module";
+import { MemberService } from "../member/member.service";
+import { MemberModule } from "../member/member.module";
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }]), AuthModule, ViewModule],
-	providers: [PropertyResolver, PropertyService],
+	imports: [MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }]), AuthModule, ViewModule, MemberModule],
+  providers: [PropertyResolver, PropertyService],
+  exports:[MemberService],
 })
 export class PropertyModule {}
