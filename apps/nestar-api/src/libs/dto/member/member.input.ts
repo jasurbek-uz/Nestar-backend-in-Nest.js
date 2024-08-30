ea07df91876207e5ae7c5a5ed112dd0cd70798af
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-08-30 22:01:04 +0900

    feat:bugs are fixed

--- apps/nestar-api/src/libs/dto/member/member.input.ts
+++ apps/nestar-api/src/libs/dto/member/member.input.ts
@@ -1,7 +1,7 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
 import { IsIn, IsNotEmpty, IsOptional, Length, Min } from 'class-validator';
 import { MemberAuthType, MemberStatus, MemberType } from '../../enums/member.enum';
-import { availableAgentSorts, availableMembertSorts } from '../../config';
+import { availableAgentSorts, availableMemberSorts } from '../../config';
 import { Direction } from '../../enums/common.enum';
 
 @InputType()
@@ -103,7 +103,7 @@ export class MembersInquiry {
 	limit: number;
 
 	@IsOptional()
-	@IsIn([availableMembertSorts])
+	@IsIn([availableMemberSorts])
 	@Field(() => String, { nullable: true })
 	sort?: string;
 
