490142cb9c058fb9b9166edf3fa7f4400e509f7f
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-09-03 16:00:04 +0900

    feat:develop removePropertyByAdmin graphql api

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -12,53 +12,52 @@ import { ViewService } from "../view/view.service";
 import { PropertyUpdate } from "../../libs/dto/property/property.update";
 import  * as moment from "moment";
 import { lookupMember, shapeIntoMongoObjectId } from "../../libs/config";
-import { ViewInput } from "../../libs/dto/view/view.input";
 
 @Injectable()
 export class PropertyService {
-	constructor(
-		@InjectModel('Property') private readonly propertyModel: Model<Property>,
-		private memberService: MemberService,
-		private viewService: ViewService,
-	) {}
+  constructor(
+    @InjectModel('Property') private readonly propertyModel: Model<Property>,
+    private memberService: MemberService,
+    private viewService: ViewService,
+  ) { }
 
-	public async createProperty(input: PropertyInput): Promise<Property> {
-		try {
-			const result = await this.propertyModel.create(input);
-			// increase memberProperties
-			await this.memberService.memberStatsEditor({ _id: result.memberId, targetKey: 'memberProperties', modifier: 1 });
-			return result;
-		} catch (err) {
-			console.log('Error, createProperty:', err.message);
-			throw new BadRequestException(Message.CREATE_FAILED);
-		}
-	}
+  public async createProperty(input: PropertyInput): Promise<Property> {
+    try {
+      const result = await this.propertyModel.create(input);
+      // increase memberProperties
+      await this.memberService.memberStatsEditor({ _id: result.memberId, targetKey: 'memberProperties', modifier: 1 });
+      return result;
+    } catch (err) {
+      console.log('Error, createProperty:', err.message);
+      throw new BadRequestException(Message.CREATE_FAILED);
+    }
+  }
 
-	public async getProperty(memberId: ObjectId, propertyId: ObjectId): Promise<Property> {
-		const search: T = {
-			_id: propertyId,
-			PropertyStatus: PropertyStatus.ACTIVE,
-		};
-		const targetProperty: Property = await this.propertyModel.findOne(search).lean().exec();
-		if (!targetProperty) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
-		if (memberId) {
-			const viewInput = { memberId: memberId, viewRefId: propertyId, viewGroup: ViewGroup.PROPERTY };
-			const newView = await this.viewService.recordView(viewInput);
-			if (newView) {
-				await this.propertyStatsEditor({ _id: propertyId, targetKey: 'propertyViews', modifier: 1 });
-				targetProperty.propertyViews++;
-			}
-		}
-		targetProperty.memberData = await this.memberService.getMember(null, targetProperty.memberId);
-		return targetProperty;
+  public async getProperty(memberId: ObjectId, propertyId: ObjectId): Promise<Property> {
+    const search: T = {
+      _id: propertyId,
+      propertyStatus: PropertyStatus.ACTIVE,
+    };
+    const targetProperty: Property = await this.propertyModel.findOne(search).lean().exec();
+    if (!targetProperty) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+    if (memberId) {
+      const viewInput = { memberId: memberId, viewRefId: propertyId, viewGroup: ViewGroup.PROPERTY };
+      const newView = await this.viewService.recordView(viewInput);
+      if (newView) {
+        await this.propertyStatsEditor({ _id: propertyId, targetKey: 'propertyViews', modifier: 1 });
+        targetProperty.propertyViews++;
+      }
+    }
+    targetProperty.memberData = await this.memberService.getMember(null, targetProperty.memberId);
+    return targetProperty;
   }
   
-	public async propertyStatsEditor(input: StatisticModifier): Promise<Property> {
-		const { _id, targetKey, modifier } = input;
-    return await this.propertyModel.findOneAndUpdate(_id, { $inc: { [targetKey]: modifier } }, { new: true }) // findByIdAndUpdate Bekzod akani darsida  
+  public async propertyStatsEditor(input: StatisticModifier): Promise<Property> {
+    const { _id, targetKey, modifier } = input;
+    return await this.propertyModel.findByIdAndUpdate(_id, { $inc: { [targetKey]: modifier } }, { new: true })
       .exec();
   }
-  
+
   public async updateProperty(memberId: ObjectId, input: PropertyUpdate): Promise<Property>{
     let { propertyStatus, soldAt, deletedAt } = input;
     const search: T = {
