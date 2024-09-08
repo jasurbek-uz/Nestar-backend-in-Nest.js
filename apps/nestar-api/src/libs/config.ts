90b21bb4d088998b2653487f9038e62dda1b14a1
Jasurbek Ergashaliev <130834808+jasurbek-uz@users.noreply.github.com>
2024-09-08 21:40:57 +0900

    Update config.ts

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,13 +1,22 @@
 import { ObjectId } from 'bson';
 
-export const availableAgentSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews", "memberRank"]
+export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank'];
 export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
+
 export const availableOptions = ['propertyBarter', 'propertyRent'];
-export const availablePropertySorts = ['createdAt', 'updatedAt', 'propertyLikes', 'propertyViews', 'propertyRank', 'propertyPrice',];
-export const availableBoardArticleSorts = [' creatdAt', 'updatedAt', 'articleLikes', 'articleViews'];
+export const availablePropertySorts = [
+	'createdAt',
+	'updatedAt',
+	'propertyLikes',
+	'propertyViews',
+	'propertyRank',
+	'propertyPrice',
+];
+
+export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
 export const availableCommentSorts = ['createdAt', 'updatedAt'];
 
- // IMAGE CONFIGURATION (config.js)
+/**  IMAGE CONFIGURATION **/
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
 
@@ -18,14 +27,14 @@ export const getSerialForImage = (filename: string) => {
 };
 
 export const shapeIntoMongoObjectId = (target: any) => {
-  return typeof target === "string" ? new ObjectId(target) : target;
-}
+	return typeof target === 'string' ? new ObjectId(target) : target;
+};
 
 export const lookupMember = {
-  $lookup: {
-    from: 'members',
-    localField: 'memberId',
-    foreignField: '_id',
-    as: 'memberData',
-  },
+	$lookup: {
+		from: 'members',
+		localField: 'memberId',
+		foreignField: '_id',
+		as: 'memberData',
+	},
 };
