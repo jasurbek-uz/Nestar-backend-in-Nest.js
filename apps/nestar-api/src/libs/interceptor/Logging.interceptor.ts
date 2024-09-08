aead615ab8f39273ebf0ac2eaacef3a8d7cd944a
Jasurbek Ergashaliev <130834808+jasurbek-uz@users.noreply.github.com>
2024-09-08 21:40:27 +0900

    Update Logging.interceptor.ts

--- apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
+++ apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
@@ -1,39 +1,49 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
-import { GqlContextType, GqlExecutionContext } from "@nestjs/graphql";
-import { stringify } from "querystring";
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
+import { Observable, tap } from 'rxjs';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  private readonly logger: Logger = new Logger();
+	private readonly logger: Logger = new Logger();
 
- public intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const recordTime = Date.now();
-    // const type = context.getType();
-    const requestType = context.getType<GqlContextType>();
-    
-    if (requestType === "http") {
-      // Develop if needed! 
-    } else if (requestType === 'graphql') {
-      // (1) Print request
-      const gqlContext = GqlExecutionContext.create(context);
-      console.log('gqlContext =>', gqlContext.getContext().req.body);
-      this.logger.log(`${this.stringify(gqlContext.getContext().req.body)}`, 'REQUEST')
-      // (2) Errors handling via Graphql
-      
-      // (3) No errors giving Response below
-    return next.handle().pipe(
-      tap((context) => {
-        const responseTime = Date.now() - recordTime;
-        this.logger.log(`${this.stringify(context)} -${responseTime}ms \n\n`, "RESPONSE")
-      }),
-    );
+	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+		const recordTime = Date.now();
+		const requestType = context.getType<GqlContextType>();
+
+		if (requestType === 'http') {
+			// Develop rest api if needed!
+			console.log('Rest Api is executed!');
+
+			return next.handle().pipe(
+				tap(() => {
+					const responseTime = Date.now() - recordTime;
+					this.logger.log(`${responseTime}ms`, 'RESPONSE');
+					console.log('--------------------------------------------------------');
+				}),
+			);
+		} else if (requestType === 'graphql') {
+			// (1) => Print Request
+
+			const gqlContext = GqlExecutionContext.create(context);
+
+			console.log('--------------------------------------------------------');
+			console.log('GraphQL Api is executed!');
+			this.logger.log(`${this.stringify(gqlContext.getContext().req.body)}`, 'REQUEST');
+
+			// (2) => Errors handling via GraphQL!
+
+			// (3) => Gives response below if no errors!
+			return next.handle().pipe(
+				tap((context) => {
+					const responseTime = Date.now() - recordTime;
+					this.logger.log(`${this.stringify(context)} - ${responseTime}ms \n\n`, 'RESPONSE');
+					console.log('--------------------------------------------------------');
+				}),
+			);
 		}
-  }
-  
-  private stringify(context: ExecutionContext): string{
-    console.log(typeof context);
-    return JSON.stringify(context).slice(0, 75);
-  }
+	}
+
+	private stringify(context: ExecutionContext): string {
+		return JSON.stringify(context).slice(0, 75);
+	}
 }
