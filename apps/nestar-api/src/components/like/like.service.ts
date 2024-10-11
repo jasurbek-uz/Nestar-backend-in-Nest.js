16a0d382e5849b3f41ee777685ff824a01f23f40
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-10-11 20:47:10 +0900

    feat: bugs are fixed

--- apps/nestar-api/src/components/like/like.service.ts
+++ apps/nestar-api/src/components/like/like.service.ts
@@ -1,24 +1,23 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { Like, MeLiked } from "../../libs/dto/like/like";
-import { Model } from "mongoose";
-import { InjectModel } from "@nestjs/mongoose";
-import { LikeInput } from "../../libs/dto/like/like.input";
-import { T } from "../../libs/types/common";
-import { Message } from "../../libs/enums/common.enum";
-import { ObjectId } from "mongoose";
-import { OrdinaryInquiry } from "../../libs/dto/property/property.input";
-import { LikeGroup } from "../../libs/enums/like.enum";
-import { Properties } from "../../libs/dto/property/property";
-import { lookupFavorite } from "../../libs/config";
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { Like, MeLiked } from '../../libs/dto/like/like';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { T } from '../../libs/types/common';
+import { Message } from '../../libs/enums/common.enum';
+import { OrdinaryInquiry } from '../../libs/dto/property/property.input';
+import { Properties } from '../../libs/dto/property/property';
+import { LikeGroup } from '../../libs/enums/like.enum';
+import { lookupFavoriteData } from '../../libs/config';
 
 @Injectable()
 export class LikeService {
 	constructor(@InjectModel('Like') private readonly likeModel: Model<Like>) {}
 
 	public async toggleLike(input: LikeInput): Promise<number> {
-		const search: T = { memberId: input.memberId, likeRefId: input.likeRefId },
-			exist = await this.likeModel.findOne(search).exec();
-		let modifier = 1;
+		const search: T = { memberId: input.memberId, likeRefId: input.likeRefId };
+		const exist = await this.likeModel.findOne(search).exec();
+		let modifier: number = 1;
 
 		if (exist) {
 			await this.likeModel.findOneAndDelete(search).exec();
@@ -27,17 +26,23 @@ export class LikeService {
 			try {
 				await this.likeModel.create(input);
 			} catch (err) {
-				console.log('Error, Service.model:', err.message);
+				console.log('Error on toggleLike Service =>', err);
 				throw new BadRequestException(Message.CREATE_FAILED);
 			}
 		}
-		console.log(`-Like modifier ${modifier} -`);
+
 		return modifier;
 	}
 
 	public async checkLikeExistence(input: LikeInput): Promise<MeLiked[]> {
 		const { memberId, likeRefId } = input;
-		const result = await this.likeModel.findOne({ memberId: memberId, likeRefId: likeRefId }).exec();
+		const result = await this.likeModel
+			.findOne({
+				memberId: memberId,
+				likeRefId: likeRefId,
+			})
+			.exec();
+
 		return result ? [{ memberId: memberId, likeRefId: likeRefId, myFavorite: true }] : [];
 	}
 
@@ -47,7 +52,6 @@ export class LikeService {
 
 		const data: T = await this.likeModel
 			.aggregate([
-				//likeloglarni beradi bizga bu qismi
 				{ $match: match },
 				{ $sort: { updatedAt: -1 } },
 				{
@@ -58,13 +62,15 @@ export class LikeService {
 						as: 'favoriteProperty',
 					},
 				},
-				{ $unwind: '$favoriteProperty' },
+				{
+					$unwind: '$favoriteProperty',
+				},
 				{
 					$facet: {
 						list: [
 							{ $skip: (page - 1) * limit },
 							{ $limit: limit },
-							lookupFavorite,
+							lookupFavoriteData,
 							{ $unwind: '$favoriteProperty.memberData' },
 						],
 						metaCounter: [{ $count: 'total' }],
@@ -72,8 +78,15 @@ export class LikeService {
 				},
 			])
 			.exec();
-		const result: Properties = { list: [], metaCounter: data[0].metaCounter };
-		result.list = data[0].list.map((ele) => ele.favoriteProperty);
+
+		const result: Properties = {
+			list: [],
+			metaCounter: data[0].metaCounter,
+		};
+
+		result.list = data[0].list.map((ele: T) => ele.favoriteProperty);
+
+		// console.log("result on getFavoriteProperties =>", result);
 		return result;
 	}
 }
--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,4 +1,7 @@
 import { ObjectId } from 'bson';
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+import { T } from './types/common';
 
 export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank'];
 export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
@@ -14,91 +17,21 @@ export const availablePropertySorts = [
 ];
 
 export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
+
 export const availableCommentSorts = ['createdAt', 'updatedAt'];
 
-/**  IMAGE CONFIGURATION **/
-import { v4 as uuidv4 } from 'uuid';
-import * as path from 'path';
-import { T } from './types/common';
-
+// IMAGE CONFIGURATION
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
 export const getSerialForImage = (filename: string) => {
 	const ext = path.parse(filename).ext;
 	return uuidv4() + ext;
 };
 
-export const shapeIntoMongoObjectId = (target: any) => {
+export const shapeIntoMongooseObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
 };
 
-export const lookupAuthMemberLiked = (memberId: T, targetRefId: string = '$_id') => {
-	return {
-		$lookup: {
-			from: 'likes',
-			let: {
-				localLikeRefId: targetRefId,
-				localMemberId: memberId,
-				localMyFavorite: true,
-			},
-			pipeLine: [
-				{
-					$match: {
-						$expr: {
-							$and: [{ $eq: ['$likeRefId', '$$localLikeRefId'] }, { $eq: ['$memberId', '$$localMemberId'] }],
-						},
-					},
-				},
-				{
-					$project: {
-						_id: 0,
-						memberId: 1,
-						likeRefId: 1,
-						myFavorite: '$$localMyFavorite',
-					},
-				},
-			],
-			as: 'meLiked',
-		},
-	};
-};
-
-interface LookupAuthMemberFollowed {
-	followerId: T;
-	followingId: string;
-}
-export const lookupAuthMemberFollowed = (input: LookupAuthMemberFollowed) => {
-	const { followingId, followerId } = input;
-	return {
-		$lookup: {
-			from: 'follows',
-			let: {
-				localFollowerId: followerId,
-				localFollowingId: followingId,
-				localMyFavorite: true,
-			},
-			pipeLine: [
-				{
-					$match: {
-						$expr: {
-							$and: [{ $eq: ['$followerId', '$$localFollowerId'] }, { $eq: ['$followingId', '$$localFollowingId'] }],
-						},
-					},
-				},
-				{
-					$project: {
-						_id: 0,
-						followerId: 1,
-						followingId: 1,
-						myFollowing: '$$localMyFavorite',
-					},
-				},
-			],
-			as: 'meFollowed',
-		},
-	};
-};
-
-export const lookupMember = {
+export const lookupMemberData = {
 	$lookup: {
 		from: 'members',
 		localField: 'memberId',
@@ -125,7 +58,7 @@ export const lookupFollowerData = {
 	},
 };
 
-export const lookupFavorite = {
+export const lookupFavoriteData = {
 	$lookup: {
 		from: 'members',
 		localField: 'favoriteProperty.memberId',
@@ -134,11 +67,88 @@ export const lookupFavorite = {
 	},
 };
 
-export const lookupVisit = {
+export const lookupVisitData = {
 	$lookup: {
 		from: 'members',
 		localField: 'visitedProperty.memberId',
 		foreignField: '_id',
 		as: 'visitedProperty.memberData',
 	},
+};
+
+// ======= Complex Lookup Aggregation =======
+export const lookupAuthMemberLiked = (memberId: T, targetRefId: string = '$_id') => {
+	return {
+		$lookup: {
+			from: 'likes',
+			let: {
+				localLikeRefId: targetRefId,
+				localMemberId: memberId,
+				localMyFavorite: true,
+			},
+			pipeline: [
+				{
+					$match: {
+						$expr: {
+							$and: [{ $eq: ['$likeRefId', '$$localLikeRefId'] }, { $eq: ['$memberId', '$$localMemberId'] }],
+						},
+					},
+				},
+				{
+					$project: {
+						_id: 0,
+						memberId: 1,
+						likeRefId: 1,
+						myFavorite: '$$localMyFavorite',
+					},
+				},
+			],
+			as: 'meLiked',
+		},
+	};
+};
+
+interface LookupAuthMemberFollowed {
+	followerId: T;
+	followingId: string;
+}
+
+export const lookupAuthMemberFollowed = (input: LookupAuthMemberFollowed) => {
+	const { followerId, followingId } = input;
+
+	return {
+		$lookup: {
+			from: 'follows',
+			let: {
+				localFollowerId: followerId,
+				localFollowingId: followingId,
+				localMyFavorite: true,
+			},
+			pipeline: [
+				{
+					$match: {
+						$expr: {
+							$and: [
+								{
+									$eq: ['$followerId', '$$localFollowerId'],
+								},
+								{
+									$eq: ['$followingId', '$$localFollowingId'],
+								},
+							],
+						},
+					},
+				},
+				{
+					$project: {
+						_id: 0,
+						followerId: 1,
+						followingId: 1,
+						myFollowing: '$$localMyFavorite',
+					},
+				},
+			],
+			as: 'meFollowed',
+		},
+	};
 };
